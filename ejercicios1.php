0fcb770ac6eca05062c179e8ca3b4d66636a3092
Sergio <sergiosanchez.24@campuscamara.es>
2025-10-08 09:36:30 +0200

    init

---
+++ ejercicios1.php
@@ -0,0 +1,11 @@
+<?php
+$nombre="Sergio";//Declaro la variable nombre, que es igual a Sergio
+echo "Hola, mi nombre es " .$nombre;//Escribe por pantalla "Hola, mi nombre es " y llamo a la variable
+
+<br></br>
+define("PI", 3.1415);
+echo PI;//Escribe "3.1415" por pantalla
+
+$nombre2="sergio";
+echo "Hola, " .$nombre, " y " .$nombre2;
+?>
